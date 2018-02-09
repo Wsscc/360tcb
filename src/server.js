37fe137a99fa06b8fc04d8269ea47625e2c7a8da
wangyihang <861045734@qq.com>
2018-02-09 13:46:03 +0800

    DAY2

---
+++ src/server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var fs = require("fs");
+var http = express();
+http.listen(8080,function(){
+	console.log("服务已开启")
+})
+http.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin","*");
+	next();
+})
